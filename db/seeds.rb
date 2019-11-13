453060d21f2bdf710cd832f56622671509e26586
Theodore JUDLIN <theodore.judlin@free.fr>
2019-11-14 00:12:21 +0100

    avec correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '05 05 03 03 03',
+    category: 'italian'
+  },
+  {
+    name:         'brigade',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '05 05 03 03 03',
+    category: 'japanese'
+  },
+    {
+    name:         'Pierre',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '05 05 03 03 03',
+    category: 'french'
+  },
+    {
+    name:         'Paul',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '05 05 03 03 03',
+    category: 'japanese'
+  },
+    {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '05 05 03 03 03',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+# puts 'Cleaning database...'
+# Reviews.destroy_all
+
+# puts 'Creating reviews...'
+# reviews_attributes = [
+#   {
+#     content:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+#     rating:        5
+#     restaurant: 1
+#   },
+#   {
+#     content:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+#     rating:        4
+#     restaurant: 2
+# }
+
+# Review.create!(reviews_attributes)
+# puts 'Finished!'
